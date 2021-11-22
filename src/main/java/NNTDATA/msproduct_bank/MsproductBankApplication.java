9c9edfa2b14a2fff51615e8679dc863330041cf6
Dtravezano <Davidu.0622@gmail.com>
2021-11-22 11:15:23 -0500

    refactor start project

---
+++ src/main/java/NNTDATA/msproduct_bank/MsproductBankApplication.java
@@ -0,0 +1,13 @@
+package NNTDATA.msproduct_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsproductBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsproductBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NNTDATA/msproduct_bank/MsproductBankApplicationTests.java
@@ -0,0 +1,13 @@
+package NNTDATA.msproduct_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsproductBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
