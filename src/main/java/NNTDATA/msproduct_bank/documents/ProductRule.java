9858925cc5144ece14d4c3bfbd2f74c9c19a6bdf
Dtravezano <Davidu.0622@gmail.com>
2021-12-07 17:11:28 -0500

    refcator

--- src/main/java/NNTDATA/msproduct_bank/documents/ProductRule.java
+++ src/main/java/NNTDATA/msproduct_bank/documents/ProductRule.java
@@ -1,19 +1,25 @@
 package NNTDATA.msproduct_bank.documents;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Builder
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRule {
+    @Id
+    private String id;
     private List<String> customerOwner;
     private boolean commissionMaintenance;
     private boolean maximumLimitMonthlyMovements;
     private Integer quantityMaximumMovementsMonthly;
     private Integer maximumLimitCreditPerson;
     private Integer maximumLimitCreditEnterprise;
-    private boolean balance;
+    private Integer balance;
 }
--- src/main/java/NNTDATA/msproduct_bank/services/ProductServiceImpl.java
+++ src/main/java/NNTDATA/msproduct_bank/services/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package NNTDATA.msproduct_bank.services;
 import NNTDATA.msproduct_bank.dao.ProductDao;
 import NNTDATA.msproduct_bank.documents.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ProductServiceImpl implements ProductService{
 
     @Autowired
